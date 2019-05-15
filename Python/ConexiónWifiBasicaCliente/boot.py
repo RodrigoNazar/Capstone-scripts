24a5c8aacd8d634332bcc59dfded97727b09060f
RodrigoNazar <rnazar@uc.cl>
2019-05-15 17:33:30 -0400

    un commit pal mathi

--- Python/ConexiónWifiBasicaCliente/boot.py
+++ Python/ConexiónWifiBasicaCliente/boot.py
@@ -14,12 +14,12 @@ def do_connect():
     import network
     sta_if = network.WLAN(network.STA_IF)
     if not sta_if.isconnected():
-        print('connecting to network...')
+        print('\nConnecting to network...')
         sta_if.active(True)
         sta_if.connect('iPhone de Rodrigo', 'patricio')
         while not sta_if.isconnected():
             pass
-    print('\nnetwork config:', sta_if.ifconfig()[0])
+    print('\nConnected!\n')
 
 
 do_connect()
--- Python/ConexiónWifiBasicaCliente/main.py
+++ Python/ConexiónWifiBasicaCliente/main.py
@@ -1,5 +1,6 @@
 # from server import set_up_server
 import socket
+from machine import Pin, PWM
 
 addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
 
@@ -7,12 +8,10 @@ s = socket.socket()
 s.bind(addr)
 s.listen(1)
 
-print('\n\nlistening on', addr)
+# print('\n\nlistening on', addr)
 
 while True:
     cl, addr = s.accept()
-    # print('Client connected from', addr)
     data = cl.recv(1024).decode('ascii')
-    print('Data collected:', data)
-    # cl.sendall(data)
+    # print('Data collected:', data)
     cl.close()
