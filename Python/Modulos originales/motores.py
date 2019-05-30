081dd348bf87a6ce73b2586c8a5774472c077d91
mathias lambert <mathiaslambert@MacBook-Pro.local>
2019-05-30 17:02:35 -0400

    new motor

--- Python/Modulos originales/motores.py
+++ Python/Modulos originales/motores.py
@@ -4,31 +4,26 @@ import time
 
 class Motores:
     def __init__(self, pin1=12, pin2=13, pin3=0, pin4=2):
-        # self._duty1 = 58
-        # self._duty2 = 58
-        # self._duty3 = 58
-        # self._duty4 = 51
-        self._duty1 = 341
-        self._duty2 = 341
-        self._duty3 = 341
-        self._duty4 = 341
-        # self.motor1 = PWM(Pin(pin1), freq=50, duty=self._duty1)
-        # self.motor2 = PWM(Pin(pin2), freq=50, duty=self._duty2)
-        # self.motor3 = PWM(Pin(pin3), freq=50, duty=self._duty3)
-        # self.motor4 = PWM(Pin(pin4), freq=50, duty=self._duty4)
-        self.motor1 = PWM(Pin(pin1), freq=166, duty=self._duty1)
-        self.motor2 = PWM(Pin(pin2), freq=166, duty=self._duty2)
-        self.motor3 = PWM(Pin(pin3), freq=166, duty=self._duty3)
-        self.motor4 = PWM(Pin(pin4), freq=166, duty=self._duty4)
+
+        self.max = 1000
+        self.min = 100
+
+        self._duty1 = 100
+        self._duty2 = 100
+        self._duty3 = 100
+        self._duty4 = 100
+
+        self.motor1 = PWM(Pin(pin1), freq=24000, duty=self.max)
+        self.motor2 = PWM(Pin(pin2), freq=24000, duty=self.max)
+        self.motor3 = PWM(Pin(pin3), freq=24000, duty=self.max)
+        self.motor4 = PWM(Pin(pin4), freq=24000, duty=self.max)
 
     def calibracion(self):
-        while self.duty1 > 170:
-
-            self.duty1 -= 1
-            self.duty2 -= 1
-            self.duty3 -= 1
-            self.duty4 -= 1
-            time.sleep(0.005)
+        for i in range(self.max-1,self.min-1, -1):
+            self.motor1.duty = i
+            self.motor2.duty = i
+            self.motor3.duty = i
+            self.motor4.duty = i
 
     @property
     def duty1(self):
