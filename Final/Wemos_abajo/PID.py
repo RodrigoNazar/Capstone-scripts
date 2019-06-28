5db5a318d263251dc94b7ffa9b84844a5645dcdc
mathias lambert <mathiaslambert@MacBook-Pro-3.local>
2019-06-28 01:22:05 -0400

    new PID

---
+++ Final/Wemos_abajo/PID.py
@@ -0,0 +1,45 @@
+import time
+
+
+class PID(object):
+
+    def __init__(self, kp=1, ki=0, kd=0, ref=0, ilim=(-3, 3)):
+        self.ref = ref
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self._i = 0
+        self.ilim1 = ilim[0]
+        self.ilim2 = ilim[1]
+        self.primer_ciclo = True
+        self.error_ans = 0
+        self.last = 0
+
+    def nueva_ref(self, valor):
+        self.ref = valor
+        self.primer_ciclo = True
+        self._i = 0
+        self.error_ans = 0
+
+    def calcular(self, dato):
+        now = time.ticks_ms()
+        error = self.ref - dato
+        dt = time.ticks_diff(now, self.last) / 1000
+
+        p_error = error * self.kp
+
+        if self.primer_ciclo:
+            d_error = 0
+            i_error = 0
+            self.primer_ciclo = False
+        else:
+            if self.ilim1 < dato < self.ilim2:
+                self._i += self.ki * (error + self.error_ans) * dt / 2
+
+            d_error = self.kd * (error - self.error_ans) / dt
+            i_error = self._i
+
+        self.error_ans = error
+        self.last = now
+
+        return p_error + i_error + d_error
--- Final/Wemos_abajo/main.py
+++ Final/Wemos_abajo/main.py
@@ -2,179 +2,47 @@ import time
 import mpu6050
 import micropython
 from hcsr04 import HCSR04
+from PID import PID
+
+
+def sat(valor):
+    if 42 < valor < 130:
+        return valor
+    elif valor <= 42:
+        return 42
+    else:
+        return 130
+
 
 micropython.alloc_emergency_exception_buf(100)
+
+uart = UART(0)  # init with given baudrate
+uart.init(57600, bits=8, parity=None, stop=1)  # init with given parameters
 
 sensor = HCSR04(trigger_pin=14, echo_pin=12)
 
 mpu = mpu6050.MPU()
 mpu.calibrate()
 
+roll = PID(kp=1, ki=0.1, kd=0.2)
 
-while 1:
+pwm1 = 60
+pwm2 = 60
+uart.write(bytes([0]))
+time.sleep(1)
+uart.write(bytes([1]))
+uart.write(bytes([pwm1]))
+uart.write(bytes([2]))
+uart.write(bytes([pwm2]))
 
-    init = time.ticks_ms()
+while True:
     medicion = mpu.read_position()
     filtro = medicion[0]
-    distance = sensor.distance_cm()
-
-    a = 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a *= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-    a /= 2
-
-    print(filtro[0])
-    print(filtro[1])
-    print(distance)
+    # distance = sensor.distance_cm()
+    d = roll.calcular(filtro[0])
+    pwm1 = sat(pwm1 + int(d / 2))
+    pwm2 = sat(pwm2 - int(d / 2))
+    uart.write(bytes([1]))
+    uart.write(bytes([pwm1]))
+    uart.write(bytes([2]))
+    uart.write(bytes([pwm2]))
