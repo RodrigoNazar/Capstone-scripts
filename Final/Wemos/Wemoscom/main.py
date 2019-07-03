9ce835d577f11ac54ed6d0e6ca34db0a35a8f60d
mathias lambert <mathiaslambert@MacBook-Pro-3.local>
2019-07-03 18:26:58 -0400

    d0 to d3

--- Final/Wemos/Wemoscom/main.py
+++ Final/Wemos/Wemoscom/main.py
@@ -4,13 +4,13 @@ import utime
 
 freq(160000000)
 
-d3 = Pin(0, Pin.OUT) #D0
+d3 = Pin(0, Pin.OUT) #D3
 d5 = Pin(14, Pin.OUT) #D5
 d6 = Pin(12, Pin.OUT) #D6
 d7 = Pin(13, Pin.OUT) #D7
 d8 = Pin(15, Pin.OUT) #D8
 
-d0.value(0)
+d3.value(0)
 d5.value(0)
 d6.value(0)
 d7.value(0)
@@ -28,9 +28,9 @@ while True:
     data = cl.recv(1024)
 
     if data == 1:
-        d0.value(0)
+        d3.value(0)
         utime.sleep_us(10)
-        d0.value(1)
+        d3.value(1)
     elif data == 2:
         d5.value(0)
         utime.sleep_us(10)
--- Final/Wemos/k_sub.py
+++ Final/Wemos/k_sub.py
@@ -9,4 +9,21 @@ def imprimir():
                     f.write("[{0}, {1}, {2}],\n".format(kp, ki, kd))
         f.write(']')
 
-imprimir()
+from k import *
+
+print(constantes[5])
+print(constantes[10])
+print(constantes[15])
+print(constantes[20])
+print(constantes[39])
+print(constantes[44])
+print(constantes[49])
+print(constantes[54])
+print(constantes[61])
+print(constantes[66])
+print(constantes[71])
+print(constantes[76])
+print(constantes[81])
+print(constantes[86])
+print(constantes[91])
+print(constantes[106])
